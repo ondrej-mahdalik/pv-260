acfa4646f1c96fa5283b9517826174ae0fd3c4ed
Ondřej Mahdalík <49830945+ondrej-mahdalik@users.noreply.github.com>
2025-03-17 08:03:00 +0100

    Add empty projects, run Unit tests in pull requests (#1)

---
+++ src/PV260.API.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace PV260.API.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ src/PV260.Client.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace PV260.Client.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
