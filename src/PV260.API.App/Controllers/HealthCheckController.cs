825f8599980dab02199c4b1e10e8003e73d32184
Ondřej Mahdalík <49830945+ondrej-mahdalik@users.noreply.github.com>
2025-03-30 16:03:22 +0200

    IN-35 Specify API endpoints (#10)

---
+++ src/PV260.API.App/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PV260.API.App.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<string> Get()
+    {
+        return Ok("Healthy");
+    }
+}
---
+++ src/PV260.API.App/Controllers/ReportController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PV260.Common.Models;
+
+namespace PV260.API.App.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReportController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ReportModel>>> GetAllReports()
+    {
+        return Ok(new List<ReportModel>());
+    }
+    
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ReportModel>> GetReportById(Guid id)
+    {
+        return Ok(new ReportModel());
+    }
+    
+    [HttpGet("latest")]
+    public async Task<ActionResult<ReportModel>> GetLatestReport()
+    {
+        return Ok(new ReportModel());
+    }
+    
+    [HttpPost("generate")]
+    public async Task<ActionResult<ReportModel>> GenerateNewReport()
+    {
+        return Ok(new ReportModel());
+    }
+    
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> DeleteReport(Guid id)
+    {
+        return NoContent();
+    }
+    
+    [HttpDelete("all")]
+    public async Task<ActionResult> DeleteAllReports()
+    {
+        return NoContent();
+    }
+    
+    [HttpPost("{id:guid}/send")]
+    public async Task<ActionResult> SendReport(Guid id)
+    {
+        return Ok();
+    }
+}
