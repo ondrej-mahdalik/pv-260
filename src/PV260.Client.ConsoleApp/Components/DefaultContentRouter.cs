f23dc8fc0cc177ae568145c5f00f4de263367f96
Matej Keznikl <matej.keznikl@outlook.com>
2025-04-23 23:19:07 +0200

    ADD: api client to route

--- src/PV260.Client.ConsoleApp/Components/DefaultContentRouter.cs
+++ src/PV260.Client.ConsoleApp/Components/DefaultContentRouter.cs
@@ -1,12 +1,14 @@
-﻿using PV260.Client.ConsoleApp.Components.Content;
+﻿using PV260.Client.BL;
+using PV260.Client.ConsoleApp.Components.Content;
 using PV260.Client.ConsoleApp.Components.Content.Emails;
 using PV260.Client.ConsoleApp.Components.Content.Reports;
+using PV260.Client.ConsoleApp.Components.Content.Reports.LatestGeneratedReport;
 using PV260.Client.ConsoleApp.Components.Enums;
 using PV260.Client.ConsoleApp.Components.Interfaces;
 
 namespace PV260.Client.ConsoleApp.Components;
 
-internal class DefaultContentRouter : IContentRouter
+internal class DefaultContentRouter(IApiClient apiClient) : IContentRouter
 {
     public IRenderableComponent Route(MenuItems menuItem)
     {
