9324fc22532dde54577e6e85ebb4804972886087
Matej Keznikl <matej.keznikl@outlook.com>
2025-04-26 17:59:09 +0200

    FIX: Navigation in console application

--- src/PV260.Client.ConsoleApp/ConsoleApplication.cs
+++ src/PV260.Client.ConsoleApp/ConsoleApplication.cs
@@ -1,6 +1,5 @@
 ﻿using PV260.Client.ConsoleApp.Components.Enums;
 using PV260.Client.ConsoleApp.Components.Interfaces;
-using PV260.Client.ConsoleApp.Components.Navigation;
 using PV260.Client.ConsoleApp.Components.Navigation.Interfaces;
 using Spectre.Console;
 
@@ -13,12 +12,13 @@ internal class ConsoleApplication(
 {
     private readonly IContentRouter _contentRouter = contentRouter;
     private readonly ILayoutBuilder _layoutBuilder = layoutBuilder;
-    private readonly INavigationService _navigationService = navigationService;
 
     private readonly MenuItems[] _mainMenuItems =
     [
         MenuItems.Home, MenuItems.LatestGeneratedReport, MenuItems.Reports, MenuItems.Emails, MenuItems.About
     ];
+
+    private readonly INavigationService _navigationService = navigationService;
 
     private int _mainSelected;
     private bool _running = true;
@@ -59,20 +59,38 @@ internal class ConsoleApplication(
             return;
         }
 
-        if (!component.IsInSubMenu)
+        switch (component)
         {
-            HandleMainNavigation(key);
-            return;
-        }
+            case INavigationComponent navigationComponent:
+            {
+                navigationComponent.Navigate(key.Key);
+                navigationComponent.HandleInput(key.Key, _navigationService);
 
-        if (component is INavigationComponent navComponent)
-        {
-            navComponent.Navigate(key.Key);
-            navComponent.HandleInput(key, _navigationService);
-        }
-        else if (component is IContentComponent contentComponent)
-        {
-            contentComponent.HandleInput(key);
+                if (key.Key != ConsoleKey.Backspace)
+                {
+                    return;
+                }
+
+                if (_navigationService.CanGoBack)
+                {
+                    _navigationService.Pop();
+                }
+
+                break;
+            }
+            case IContentComponent when
+                _navigationService.LastNavigationComponent is INavigationComponent navigationComponent:
+                navigationComponent.HandleInput(key.Key, _navigationService);
+                break;
+            default:
+            {
+                if (!component.IsInSubMenu)
+                {
+                    HandleMainNavigation(key);
+                }
+
+                break;
+            }
         }
     }
 
@@ -94,6 +112,7 @@ internal class ConsoleApplication(
                 {
                     _navigationService.Pop();
                 }
+
                 break;
         }
     }
